4b4e02bb06eb31162cb913514e93434fcc50cd28
aman-satori <aman.jaiswar@satorigroup.in>
2024-10-14 11:26:35 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  3 10:16:38 2024
+
+@author: Aman Jaiswar
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='hcl',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple helper functions library for HighBond API.',
+    author='Aman Jaiswar',
+    author_email='aman.jaiswar@satorigroup.in',
+    url='https://github.com/aman-satori/HCL/blob/main/hcl.py',  # Optional
+    install_requires=[
+        'requests',
+        'requests-futures',
+        'pandas',
+        'numpy',
+        'urllib3',
+    ],  # List any dependencies here
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6'
+)
